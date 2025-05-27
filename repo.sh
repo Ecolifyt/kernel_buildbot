2de31e8d7a50da34b50f032ef1a8852583a311d7
Rafael Alvarado Navarro <ealvarado2677@gmail.com>
2025-05-26 18:10:32 -0600

    Update repo.sh

--- repo.sh
+++ repo.sh
@@ -8,6 +8,7 @@ export USE_CCACHE=1
 export CCACHE_EXEC=/usr/bin/ccache
 ccache -M 50G
 repo init -u https://github.com/RisingOS-Revived/android -b qpr2 --git-lfs --depth=1
-git clone --depth=1 https://github.com/Ecolifyt/local_manifests .repo/local_manifests
 find .repo -name '*.lock' -delete
-repo sync -c -j32 --force-sync
+repo sync -c -j32
+git clone --depth=1 https://github.com/Ecolifyt/local_manifests .repo/local_manifests
+repo sync
